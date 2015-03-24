5ef5cb4e22d5b5f6b7c5df5ec0e66ac03cbf82aa
matthew gould <gouldm44@gmail.com>
2015-03-24 16:29:13 -0400

    Initial commit with working login through github because I forgot to commit before this

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, #:registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
+
+  serialize :github_auth_data, JSON
+
+  # def self.from_omniauth auth
+  #   email = auth.info.email
+  #   raise "You must sign in with a #{DOMAIN} address" unless email.ends_with? DOMAIN
+  #   where(email: email).first_or_create! do |u|
+  #     u.password = SecureRandom.hex 64
+  #     u.google_auth_id   = auth.id
+  #     u.google_auth_data = auth.to_h
+  #   end
+  # end
+
+  def self.from_omniauth auth
+    
+  end
 end
