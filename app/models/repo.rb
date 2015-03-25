2e630debeb1e60497d3fb0931602eaa4e32acf02
matthew gould <gouldm44@gmail.com>
2015-03-25 08:45:26 -0400

    Added a new github token because I committed the last one

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,13 +1,4 @@
 class Repo < ActiveRecord::Base
   belongs_to :user
-
-  def populate_names
-    repo_list = HTTParty.get("https://api.github.com/users/#{current_user.name}/repos", 
-    headers: {"Authorization" => "token #{Figaro.env.github_token}", 
-    "User-Agent" => "#{Figaro.env.github_name}"})
-  end  
+  
 end
-
-# names = HTTParty.get("https://api.github.com/users/matthew-gould/repos", 
-#     headers: {"Authorization" => "token 36c14d7b4935689ddafa582bdaba93e25001bd57", 
-#     "User-Agent" => "matthew-gould"})
